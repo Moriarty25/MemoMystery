324d6455ee9fa8c986581f9586d9100d2109377b
Moriarty25 <afanasev.andrey99@gmail.com>
2024-07-12 15:07:51 +0300

    feature: added flipping cells

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,34 +1,111 @@
-import {useState} from 'react';
-import { useBoardState } from '../../hooks/useBoardState';
+import { useEffect, useState } from 'react';
 import { starWarsImages } from "../../modes/starWars/card";
 import { generateArray } from "../../modes/starWars/game";
 import { Cell } from "../Cell";
 import styles from "./Board.module.scss";
 import { v4 as uuidv4 } from 'uuid';
 
+export type TCell = {
+  index: number,
+  src: string,
+  open: boolean
+}
+
 export const Board = () => {
-  const [displayBoard, setDisplayBoard] = useState<string[]>([]);
-  const [openCell, setOpenCell] = useState<null | {index: number, src: string}>(null);
+  const [displayBoard, setDisplayBoard] = useState<TCell[]>([]);
+  const [flippedCards, setFlippedCards] = useState<number[]>([])
+  const [matchedCards, setMatchedCards] = useState<number[]>([]);
   const [isMatch, setIsMatch] = useState<null | 'match' | 'miss'>(null);
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  useEffect(()=> {
+    if (flippedCards.length === 2) {
+      setIsDisabled(true)
+      const [first, second] = flippedCards
+      if (displayBoard[first].src === displayBoard[second].src) {
+        setMatchedCards([...matchedCards, first, second])
+        setFlippedCards([])
+        setIsDisabled(false)
+      } else {
+        setTimeout(() => {
+          const newCards = displayBoard.map((card, index) => {
+            if (index === first || index === second) {
+              card.open = false
+            }
+            return card
+          })
+          setDisplayBoard(newCards)
+          setFlippedCards([])
+          setIsDisabled(false)
+        }, 1000)
+      }
+    } 
+  }, [displayBoard, flippedCards, matchedCards ])
 
   function onStart() {
-    setDisplayBoard(generateArray(starWarsImages, 17)); 
+    // setDisplayBoard(generateArray(starWarsImages, 17)); 
+    const newArr = generateArray(starWarsImages, 17).map((el, index) => {
+      return { src: el, index, open: false }
+    })
+    setDisplayBoard(newArr)
   }
 
-  function onFlippingBoardHandler(src: string, id: number) {
-    console.log(displayBoard.indexOf(src));
-    
-    if (src === openCell?.src) {
-      setIsMatch('match')
-    } else {
-      setIsMatch('miss')
-      setOpenCell({src, index: id}) 
-    }
+  function onFlippingBoardHandler({ src, index, open }: TCell) {
+    if (isDisabled 
+      || flippedCards.includes(index)
+      || matchedCards.includes(index)
+    ) return
+
+    const newCards = displayBoard.map((card, i) => {
+      if (i === index) {
+        card.open = true
+      }
+      return card
+    })
+    setDisplayBoard(newCards)
+    setFlippedCards([...flippedCards, index])
+    // displayBoard[index].open = true
+    const currentCard = { src, index, open: true }
+    console.log(flippedCards, currentCard);
+    // if (true) {
+    //   displayBoard[index].open = true;
+    //   console.log(flippedCards.at(-1)?.src === currentCard.src);
+
+
+    // }
+
+    // if (flippedCards.length > 1) {
+    //   const newArr = displayBoard
+    //   newArr.filter((el) => { el.index === index })
+    //   console.log(newArr, displayBoard)
+
+    //   console.log(flippedCards);
+
+    // }
+
+
+
+
+
+    // if (openCell?.[0] && src === openCell?.[0].src) {
+    //   setIsMatch('match')
+
+    // } else {
+    //   setIsMatch('miss')
+    //   setOpenCell([{ src, index: index, open: false }])
+    // }
+    // if (openCell) {
+    //   const newArr = [openCell.slice(-1), {src, index, open: true}]
+    //   setOpenCell(newArr);
+    // } else {
+    //   setOpenCell([{src, index, open: true}])
+    // }
+    //     console.log(openCell);
   }
 
   const cellsElements = displayBoard.map((cell, index) => {
-    const id = uuidv4(); 
-    return <Cell src={cell} title={'title'} index ={index} key={index} id={id} openCell={openCell} isMatch={isMatch} onClick={onFlippingBoardHandler}/>;
+    const id = uuidv4();
+    return <Cell title={'title'} key={id} cell={cell} isMatch={isMatch} onClick={onFlippingBoardHandler} />;
   });
 
   return (
@@ -39,7 +116,7 @@ export const Board = () => {
         <div className={styles.wrap}></div>
         {cellsElements}
       </main>
-      <footer>Time: 1 Score: Moves: </footer>
+      <footer>Time: 1 Score: {matchedCards.length/2} Moves: </footer>
     </div>
   );
 };
--- src/components/Cell/index.tsx
+++ src/components/Cell/index.tsx
@@ -1,24 +1,26 @@
-import { FC, useState } from "react";
+import { FC } from "react";
 import styles from "./Cell.module.scss";
+import { TCell } from "../Board";
 
 type TCellProps = {
-  src: string;
   title: string;
-  id: string;
-  openCell: null | {index: number, src: string};
+  cell: TCell;
   isMatch: 'match' | 'miss' | null;
-  onClick: (src: string, index: number) => void;
-  index: number
+  onClick: (arg: TCell) => void;
 };
 
 
 
-export const Cell: FC<TCellProps> = ({src, index, title, id, onClick, openCell, isMatch}) => {
+export const Cell: FC<TCellProps> = ({title, onClick, cell, isMatch,}) => {
   
+  const index = cell?.index
+  const open = cell?.open
+  const src = cell?.src
+ 
   return (
-    <div  onClick={() => onClick(src, index)} className={`${styles.cell} ${styles.visible} ${isMatch ==='match' ? styles.match : isMatch === 'miss' ? styles.miss : ''}`}>
-      <div className={styles.cellInner}>
-        {index === openCell?.index ? (
+    <div className={`${styles.cell} ${styles.visible} ${isMatch ==='match' ? styles.match : isMatch === 'miss' ? styles.miss : ''}`}>
+      <div onClick={ ()=> {onClick({index, src, open})}} className={styles.cellInner}>
+        {open ? (
           <img
             className={`${styles.img} ${styles.font}`}
             src={src}
