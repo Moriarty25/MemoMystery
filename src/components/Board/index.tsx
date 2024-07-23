b5c07b5f291b794a9e02bb147b77116e04da2a0f
Moriarty25 <afanasev.andrey99@gmail.com>
2024-07-23 20:12:50 +0300

    feature: added ai generation of steps

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -6,7 +6,7 @@ import styles from "./Board.module.scss";
 import { v4 as uuidv4 } from 'uuid';
 import useLocalStorage from '../../hooks/useLocalStorage';
 import { Modal } from '../Modal';
-import { Pause, PauseCircleFilledOutlined, RestartAlt } from '@mui/icons-material';
+import { Pause, RestartAlt } from '@mui/icons-material';
 import { IconButton, Tooltip } from '@mui/material';
 
 export type TCell = {
@@ -24,15 +24,53 @@ export const Board = () => {
   const [mode, setMode] = useLocalStorage('mode', 'star_wars');
   const [size, setSize] = useLocalStorage('size', 10);
   const [activeModal, setActiveModal] = useState(false);
+  const [isYourMove, setIsYouMove] = useState(true);
+  const [userScore, setUserScore] = useState(0);
+  const [aiScore, setAiScore] = useState(0);
+
 
   useEffect(() => {
-    if (matchedCards.length === 2) {
-      console.log('u win');
-      setMode('pirates')
-      console.log(mode, size);
-      
-    } 
+    if (!isYourMove && flippedCards.length === 0) {
+      const firstIndex = aiNextStep()
+      setFlippedCards([...flippedCards, firstIndex])
+      const secondIndex = aiNextStep()
+      const aiCard1 = displayBoard[secondIndex]
+      const aiCard2 = displayBoard[aiNextStep()]
+      const newCards = displayBoard.map((card, i) => {
+        if (i === aiCard1.index || i === aiCard2.index) {
+          card.open = true
+        }
+        return card
+      })
+      setDisplayBoard(newCards)
 
+      if (aiCard1.src === aiCard2.src) {
+        setMatchedCards([...matchedCards, aiCard1.index, aiCard2.index])
+        setFlippedCards([])
+        setIsDisabled(false)
+        setIsMatch('match')
+        setIsYouMove(true)
+        setAiScore(aiScore + 1)
+      } else {
+        setTimeout(() => {
+          const newCards = displayBoard.map((card, index) => {
+            if (index === aiCard1.index || index === aiCard2.index) {
+              card.open = false
+            }
+            return card
+          })
+          setDisplayBoard(newCards)
+          setFlippedCards([])
+          setIsDisabled(false)
+          setIsMatch('miss')
+          setIsYouMove(true)
+        }, 1000) 
+      }
+    }
+  }, [isYourMove])
+
+  useEffect(() => {
+    
     if (flippedCards.length === 2) {
       setIsDisabled(true)
       const [first, second] = flippedCards
@@ -41,6 +79,8 @@ export const Board = () => {
         setFlippedCards([])
         setIsDisabled(false)
         setIsMatch('match')
+        setIsYouMove(false)
+        setUserScore(userScore + 1)
       } else {
         setTimeout(() => {
           const newCards = displayBoard.map((card, index) => {
@@ -53,15 +93,25 @@ export const Board = () => {
           setFlippedCards([])
           setIsDisabled(false)
           setIsMatch('miss')
+          setIsYouMove(false)
         }, 1000)
       }
     }
-  }, [displayBoard, flippedCards, matchedCards])
+  }, [displayBoard, flippedCards, matchedCards, isYourMove])
 
+  function aiNextStep() {
+    // let randomIndex = Math.floor(Math.random() * (displayBoard.length - 1)); 
+    const freeCards: number[] = []
+    displayBoard.forEach((card) => {
+      if (!flippedCards.includes(card.index) && !matchedCards.includes(card.index)) {
+        freeCards.push(card.index)
+      }
+    })
+    const randomIndex = Math.floor(Math.random() * freeCards.length)
+    return freeCards[randomIndex]
+  }
+  
   function onStart() {
-    // setDisplayBoard(generateArray(starWarsImages, 17)); 
-    console.log('live');
-    
     const sizeBoard =  48;
     const newArr = generateArray(starWarsImages, sizeBoard).map((el, index) => {
       return { src: el, index, open: false }
@@ -125,7 +175,7 @@ export const Board = () => {
         <div className={styles.wrap}></div>
         {cellsElements}
       </main>
-      <footer>Time: 1 Score: {matchedCards.length / 2} Moves: </footer>
+      <footer>Time: 1 Score: {userScore +' : '+ aiScore} Moves: {isYourMove ? 'You' : 'AI'}</footer>
     </div>
    {activeModal && <Modal onClose={onCloseModal}/> }
     </>
